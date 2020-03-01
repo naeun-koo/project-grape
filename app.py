c8a791397f988156c6c29e87e91d7089bc4aaa2f
naeun-koo <59506897+naeun-koo@users.noreply.github.com>
2020-03-01 14:32:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,6 @@ db = client.orderlist                    # 'orderlist'라는 이름의 db를 만
 @app.route('/')
 def home():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',port=5000,debug=True)
