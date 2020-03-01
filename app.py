0a6e6239eae44eda50fbca318feb3c5adc9a2dfd
naeun-koo <59506897+naeun-koo@users.noreply.github.com>
2020-03-01 14:24:54 +0900

    서버용 파일 세팅

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.orderlist                    # 'orderlist'라는 이름의 db를 만듭니다.
+
+## URL 별로 함수명이 같거나,
+## route('/') 등의 주소가 같으면 안됩니다.
+
+@app.route('/')
+def home():
+    return render_template('index.html')
